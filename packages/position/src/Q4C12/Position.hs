9ee2da6264ce89524f414f08d9c1e6399d4cc913
quasicomputational <quasicomputational@gmail.com>
2018-01-05 13:30:36 +0000

    position: Move to 0-based internal indexing.

--- packages/position/src/Q4C12/Position.hs
+++ packages/position/src/Q4C12/Position.hs
@@ -10,7 +10,7 @@ import qualified Data.Text as ST
 import Formatting (Format, later, bprint, int)
 import GHC.Real (fromIntegral)
 
---1-based indexes. TODO: move to 0-based?
+--0-based indices.
 data Position = Position
   { posLine :: Word64
   , posColumn :: Word64
@@ -23,9 +23,9 @@ instance Ord Position where
   Position line col <= Position line' col' = line <= line' || (line == line' && col <= col')
 
 startPosition :: Position
-startPosition = Position 1 1
+startPosition = Position 0 0
 
---Note: the end's position is *not* included in the thing. So the "bar" in "foobarbaz" would be PositionRange (1, 4) (1, 7).
+--Note: the end's position is *not* included in the thing. So the "bar" in "foobarbaz" would be PositionRange (0, 3) (0, 6).
 data PositionRange = PositionRange
   { posRangeStart :: Position
   , posRangeEnd :: Position
@@ -43,13 +43,12 @@ updatePosition initial
   where
     --TODO: fromIntegral is scary. It's safe here because length >= 0, but... yeah.
     addCols t pos = Position (posLine pos) (posColumn pos + fromIntegral (ST.length t))
-    nextLine pos = Position (posLine pos + 1) 1
+    nextLine pos = Position (posLine pos + 1) 0
 
 position :: Format r (Position -> r)
 position = later $ \(Position line col) ->
-  bprint ("(" . int . ", " . int . ")") line col
+  bprint ("(" . int . ", " . int . ")") (line + 1) (col + 1)
 
 positionRange :: Format r (PositionRange -> r)
 positionRange = later $ \(PositionRange (Position line col) (Position line' col')) ->
-  bprint ("(" . int . ":" . int . "-" . int . ":" . int . ")") line col line' col'
-
+  bprint ("(" . int . ":" . int . "-" . int . ":" . int . ")") (line + 1) (col + 1) (line' + 1) (col' + 1)
