72e7d67c113a3c203a814fe7909edca9541c78ec
quasicomputational <quasicomputational@gmail.com>
2018-03-14 13:53:24 +0000

    travis: use cabal-install 2.2 across the board

--- travis/before_install.cabal-new-build.sh
+++ travis/before_install.cabal-new-build.sh
@@ -4,12 +4,4 @@ set -euxo pipefail
 
 CABALOPTS=$(./travis/options.cabal-new-build.sh)
 
-#TODO: remove the condition once we can use cabal-install 2.2 and hence new-update in all configurations
-case "$GHCSERIES" in
-  head)
-    cabal new-update $CABALOPTS
-    ;;
-  *)
-    cabal update
-    ;;  
-esac
+cabal new-update $CABALOPTS
