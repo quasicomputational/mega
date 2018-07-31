aa0cbb4ebe9431ac6a4e1fdd091ff8f57ebe5ebf
quasicomputational <quasicomputational@gmail.com>
2018-07-31 07:19:38 +0100

    travis: ditch stages, too much overhead.

--- travis/deps.stack.sh
+++ travis/deps.stack.sh
@@ -2,5 +2,4 @@
 
 set -euxo pipefail
 
-# Same logic as in deps.cabal-new-build.sh: this is only a preparatory cache-filling step, so it's okay to fail (and in fact we have to be polite and swallow the failure or else we'll block the next stage!)
-stack --no-terminal build --test --bench --only-dependencies --fast $@ || true
+stack --no-terminal build --test --bench --only-dependencies --fast $@
