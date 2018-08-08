58c99450dab45116084bc052b2ee4f779b85e22f
quasicomputational <quasicomputational@gmail.com>
2018-08-08 17:44:11 +0100

    gen-travis: tidy a little bit

--- packages/gen-travis/src/Q4C12/GenTravis.hs
+++ packages/gen-travis/src/Q4C12/GenTravis.hs
@@ -177,20 +177,20 @@ generateProjectFiles = traverseWithKey_ $ \ buildName build -> do
     , flip fmap ( buildPackages build ) $ \ package ->
         "  " <> LT.pack package
     , [ "with-compiler: ghc-" <> LT.fromStrict ( ghcVersion ( buildGHCVersion build ) ) ]
-    , if | PreRelease <- ghcRegularity $ buildGHCVersion build
-         -> [ "repository head.hackage"
-            , "  url: http://head.hackage.haskell.org/"
-            , "  secure: True"
-            , "  root-keys:"
-            , "    07c59cb65787dedfaef5bd5f987ceb5f7e5ebf88b904bbd4c5cbdeb2ff71b740"
-            , "    2e8555dde16ebd8df076f1a8ef13b8f14c66bad8eafefd7d9e37d0ed711821fb"
-            , "    8f79fd2389ab2967354407ec852cbe73f2e8635793ac446d09461ffb99527f6e"
-            , "  key-threshold: 3"
-            , ""
-            , "allow-newer: *"
-            ]
-         | otherwise
-         -> []
+    , case ghcRegularity $ buildGHCVersion build of
+        PreRelease ->
+          [ "repository head.hackage"
+          , "  url: http://head.hackage.haskell.org/"
+          , "  secure: True"
+          , "  root-keys:"
+          , "    07c59cb65787dedfaef5bd5f987ceb5f7e5ebf88b904bbd4c5cbdeb2ff71b740"
+          , "    2e8555dde16ebd8df076f1a8ef13b8f14c66bad8eafefd7d9e37d0ed711821fb"
+          , "    8f79fd2389ab2967354407ec852cbe73f2e8635793ac446d09461ffb99527f6e"
+          , "  key-threshold: 3"
+          , ""
+          , "allow-newer: *"
+          ]
+        Regular -> []
     ]
   where
     --TODO: https://github.com/haskell/containers/issues/422
