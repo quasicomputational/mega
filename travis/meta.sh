2e788c3771de584c759fa9cea2f25df9a332dbf5
quasicomputational <quasicomputational@gmail.com>
2019-05-11 11:32:40 +0100

    meta: do the stale .cabal check using a hpack binary.
    This dramatically simplifies the dependency tree.

--- travis/meta.sh
+++ travis/meta.sh
@@ -4,4 +4,18 @@ set -euxo pipefail
 
 cabal v2-run --project-file=cabal/"$PROJECT".project meta -- check-hash
 
-cabal v2-run --project-file=cabal/"$PROJECT".project meta -- check-stale-cabal
+mkdir -p "$HOME"/.local/bin
+
+HPACKVER=0.31.2
+
+curl -L https://github.com/sol/hpack/releases/download/"$HPACKVER"/hpack_linux.gz \
+  | gunzip \
+  > $HOME/.local/bin/hpack
+
+chmod +x "$HOME"/.local/bin/hpack
+
+# Run hpack on packages, and see if we're left with a clean working tree.
+# TODO: when https://github.com/sol/hpack/pull/346 is merged/released, we can do that instead of asking git!
+find packages/ -type f -path 'packages/*/package.yaml' -exec "$HOME"/.local/bin/hpack \{\} \;
+
+git diff-index --exit-code HEAD
