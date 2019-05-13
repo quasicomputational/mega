a36a151fa8ee0d7979a1f600dd806e3cd833ce77
quasicomputational <quasicomputational@gmail.com>
2019-05-13 11:38:39 +0100

    travis: instead of hashing, run gen-travis and check for dirtiness

--- travis/meta.sh
+++ travis/meta.sh
@@ -2,7 +2,10 @@
 
 set -euxo pipefail
 
-cabal v2-run --project-file=cabal/"$PROJECT".project meta -- check-hash
+# Regenerate .travis.yml and the project files, then check that none of them are different to what's in HEAD, and that there aren't any untracked files.
+cabal v2-run --project-file=cabal/"$PROJECT".project meta -- gen-travis
+
+test -z "$(git status --porcelain)"
 
 cabal v2-run --project-file=cabal/"$PROJECT".project meta -- check-defrost
 
@@ -16,7 +19,7 @@ curl -L https://github.com/sol/hpack/releases/download/"$HPACKVER"/hpack_linux.g
 
 chmod +x "$HOME"/.local/bin/hpack
 
-# Run hpack on packages, and see if we're left with a clean working tree. Note that we have to use xargs because find swallows return codes and we want to detect things that make hpack fail (e.g., hand-modification of .cabal files, bad YAML).
+# Run hpack on packages, and see if we're left with a clean working tree. Note that we have to use xargs because find swallows return codes and we want to detect things that make hpack fail (e.g., hand-modification of .cabal files, bad YAML). No need to check for untracked files here - if we had a missing .cabal file, the build wouldn't even get down here!
 # TODO: when https://github.com/sol/hpack/pull/346 is merged/released, we can do that instead of asking git! Still have to use xargs, though.
 find packages/ -type f -path 'packages/*/package.yaml' -print0 \
   | xargs -0 -n1 hpack
